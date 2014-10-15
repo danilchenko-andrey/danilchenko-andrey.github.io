a3aa6e9922f7b0abedbf4dfebc4e575735cf23e1
Andrey Danilchenko <danilchenko@yandex-team.ru>
2014-10-15 12:43:13 +0400

    Added pelican engine

---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Andrey Danilchenko'
+SITENAME = u'4ducks'
+SITEURL = 'http://localhost:8000/'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+DEFAULT_DATE_FORMAT='%A %d %B %Y'
+
+DEFAULT_LANG = 'ru'
+LOCALE='ru_RU'
+
+STATIC_PATHS = ['pictures']
+PLUGINS=['extract_toc']
+
+# theme
+THEME='notebook-theme'
+FAVICON='pictures/favicon.png'
+AVATAR='pictures/avatar.png'
+
+SITESUBTITLE='Andrey Danilchenko dev page'
+TWITTER_USERNAME='mir_nomer_nol'
+GITHUB_URL='https://github.com/danilchenko-andrey'
+
+SOCIAL=(('github', 'https://github.com/danilchenko-andrey'),
+        ('linkedin', 'https://ru.linkedin.com/in/danilchenko/'),
+        ('twitter', 'https://twitter.com/mir_nomer_nol')
+)
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
